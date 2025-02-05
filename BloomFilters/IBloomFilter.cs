e23658af8ce12e516f6899a30a8741b08449b462
alexa <alexa@LAPTOP-SMKPBBBG>
2025-02-05 12:35:22 +0000

    added Ibloomfilter

---
+++ BloomFilters/IBloomFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ShakaCoin.BloomFilters
+{
+    public interface IBloomFilter
+    {
+        int Size { get; }
+        int HashFunctions { get; }
+        BitArray BitArray { get; }
+        void AddItem(byte[] data);
+        bool ProbablyContains(byte[] data);
+
+    }
+}
---
+++ BloomFilters/OutputBloomFilter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ShakaCoin.BloomFilters
+{
+    public class OutputBloomFilter : IBloomFilter
+    {
+        public int Size => 512;
+        public int HashFunctions => 4;
+        private BitArray BitArray => new BitArray(512);
+
+        public OutputBloomFilter()
+        {
+
+        }
+
+        private int Hash(byte[] data, int seed)
+        {
+
+
+            return 0;
+        }
+
+        public void AddItem(byte[] data)
+        {
+
+        }
+
+        public bool ProbablyContains(byte[] data)
+        {
+            return false;
+        }
+
+    }
+}
